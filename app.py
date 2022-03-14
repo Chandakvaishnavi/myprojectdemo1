a496c745a755d7e97a7f4cb4cbc54f10e1ddcf90
Vaishnavi Chandak <101132266+Chandakvaishnavi@users.noreply.github.com>
2022-03-14 05:36:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from Stacks.Data_genrator import EC2
 from database.database_stack import DatabaseStack
 from Stacks.DataLoader import LoaderS3
 
-#dev_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
+dev_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 
 app = cdk.App()
-LoaderS3(app,"filesloader")
-database = Database(app, "databasestack")
-genrator = EC2(app, "datagenrator")
+LoaderS3(app,"filesloader", env=dev_env)
+database = Database(app, "databasestack", env=dev_env)
+genrator = EC2(app, "datagenrator", env=dev_env)
 genrator.add_dependency(database, "database is required to put records")
 app.synth()
